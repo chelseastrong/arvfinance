979142c109c09c269674298a811109b8a7639e95
Chelsea Strong <chelseastrong8@gmail.com>
2025-04-13 02:14:30 -0700

    input variables no record id passed yet

--- force-app/main/default/lwc/docChecklistAddDocModal/docChecklistAddDocModal.js
+++ force-app/main/default/lwc/docChecklistAddDocModal/docChecklistAddDocModal.js
@@ -1,8 +1,15 @@
-import { api } from 'lwc';
+import { api, track } from 'lwc';
 import LightningModal from 'lightning/modal';
 
 export default class MyModal extends LightningModal {
     @api content;
+    @track inputVariables = [];
+
+    get inputVariables() {
+        return [
+            { name: 'recordId', type: 'String', value: '' }
+        ];
+    }
 
     handleClose() {
         this.close('done');
